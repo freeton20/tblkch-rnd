7b73a6ff0975d8f129c520532f97b896de63536a
freeton20 <doctor4891@gmail.com>
2021-07-13 16:52:21 +0300

    disable button

--- src/Slot.js
+++ src/Slot.js
@@ -3,10 +3,12 @@ $('#playButton').click(async function () {
     $('#numbers ul').playSpin({
         manualStop: true
     });
+ 
+    $('#playButton').attr('disabled',true);
+    const startTime = performance.now();    
 
-    const startTime = performance.now();
-    
     contract.random().then(r => {
+        $('#playButton').attr('disabled',false);
         $('#numbers ul').stopSpin({ stopEndNums: [r.endNums.value0, r.endNums.value1, r.endNums.value2] });
         $('#fees').html(`Fees: ${r.fees/1e9} TON`);
         $('#timer').html(`Spent time: ${Math.round(performance.now() - startTime)} milliseconds`);
